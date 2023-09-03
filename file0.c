70e6a3da770ab9507bdd2ca0759728f94e122966
keshavjindal <kes333hav@gmail.com>
2023-09-03 00:02:11 -0400

    file0: Basic Data Types

---
+++ file0.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+#define true 1
+#define false 0
+
+// Data types in C
+
+int main()
+{
+    int a = 90;
+    printf("%d\n", a);
+
+    float b = 90.8;
+    printf("%f\n", b);
+
+    double c = -898989.0909;
+    printf("%lf\n", c);
+
+    char ch = 'k';
+    printf("%c\n", ch);
+
+    int flag = false;
+    printf("%d\n", flag);
+
+    return 0;
+}
+
